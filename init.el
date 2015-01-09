542c0f9af8c9958a75ce2dc2e8c8d225a62827f3
Pedro Gomes Tavares <pedro.tavares@locaweb.com.br>
2015-01-09 14:52:18 -0200

    Powerline

--- init.el
+++ init.el
@@ -373,3 +373,15 @@
     (lambda ()
       (interactive)
       (other-window -1)))
+
+;;; Powerline
+(add-to-list 'load-path "~/.emacs.d/vendor/emacs-powerline")
+(require 'powerline)
+
+(custom-set-faces
+ '(mode-line ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
+ '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
+
+(setq powerline-color1 "grey22")
+(setq powerline-color2 "grey40")
+(setq powerline-arrow-shape 'arrow)   ;; give your mode-line curves
