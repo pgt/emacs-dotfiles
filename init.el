fcaf8c601fee2f57dd93086ab23295e1e1f60249
Pedro Gomes Tavares <pedro.tavares@locaweb.com.br>
2015-03-11 18:45:13 -0300

    Multiple cursors and close other buffer

--- init.el
+++ init.el
@@ -628,7 +628,7 @@ Position the cursor at it's beginning, according to the current mode."
 (define-key sp-keymap (kbd "C-M-<right>") 'sp-backward-barf-sexp)
 
 ;;
-;;; splicing
+;;; splicing ;; Remove the brackets of the current block
 ;;
 (define-key sp-keymap (kbd "M-s") 'sp-splice-sexp)
 (define-key sp-keymap (kbd "M-r") 'sp-splice-sexp-killing-around)
@@ -733,3 +733,69 @@ Position the cursor at it's beginning, according to the current mode."
 
 ;; Browser ring
 (global-set-key (kbd "s-V") 'helm-show-kill-ring)
+
+;; Multiple cursors
+(eval-after-load "multiple-cursors-autoloads"
+  '(progn
+     (when (require 'multiple-cursors nil t)
+       (defun mc/mark-all-dispatch ()
+         "- add a fake cursor at current position
+
+- call mc/edit-lines if multiple lines are marked
+
+- call mc/mark-all-like-this if marked region is on a single line"
+         (interactive)
+         (cond
+          ((not (region-active-p))
+           (mc/create-fake-cursor-at-point)
+           (mc/maybe-multiple-cursors-mode))
+          ((> (- (line-number-at-pos (region-end))
+                 (line-number-at-pos (region-beginning))) 0)
+           (mc/edit-lines))
+          (t
+           (mc/mark-all-like-this))))
+
+       (defun mc/align ()
+         "Aligns all the cursor vertically."
+         (interactive)
+         (let ((max-column 0)
+               (cursors-column '()))
+           (mc/for-each-cursor-ordered
+            (mc/save-excursion
+             (goto-char (overlay-start cursor))
+             (let ((cur (current-column)))
+               (setq cursors-column (append cursors-column (list cur)))
+               (setq max-column (if (< max-column cur) cur max-column)))))
+
+           (defun mc--align-insert-times ()
+             (interactive)
+             (dotimes (_ times)
+               (insert " ")))
+           (mc/for-each-cursor-ordered
+            (let ((times (- max-column (car cursors-column))))
+              (mc/execute-command-for-fake-cursor 'mc--align-insert-times cursor))
+            (setq cursors-column (cdr cursors-column)))))
+
+       (setq mc/list-file "~/.mc-lists.el")
+       (load mc/list-file t) ;; load, but no errors if it does not exist yet please
+
+       (global-set-key (kbd "C->")  'mc/mark-next-like-this)
+       (global-set-key (kbd "C-<")  'mc/mark-previous-like-this)
+
+       (global-set-key (kbd "M-@") 'mc/mark-all-dispatch)
+       (global-set-key (kbd "M-#") 'mc/insert-numbers)
+       (global-set-key (kbd "M-'") 'mc/align))))
+
+;; Close buffer in other window
+(defun other-window-kill-buffer ()
+  "Kill the buffer in the other window"
+  (interactive)
+  ;; Window selection is used because point goes to a different window
+  ;; if more than 2 windows are present
+  (let ((win-curr (selected-window))
+        (win-other (next-window)))
+    (select-window win-other)
+    (kill-this-buffer)
+    (select-window win-curr)))
+
+(global-set-key (kbd "C-x 9") 'other-window-kill-buffer)
