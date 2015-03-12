b68b21320cc104a30c65280614a3e5c5dfdd0a0d
Pedro Gomes Tavares <pedro.tavares@locaweb.com.br>
2015-03-12 14:52:01 -0300

    Smart begin of line

--- init.el
+++ init.el
@@ -690,17 +690,17 @@ Position the cursor at it's beginning, according to the current mode."
 
 
 ;; beggining of line
-(define-key global-map [remap move-beginning-of-line]
-  (defun smart-beginning-of-line ()
-    "Move point to first non-whitespace character or beginning-of-line.
+(defun smart-beginning-of-line ()
+  "Move point to first non-whitespace character or beginning-of-line.
 
   Move point to the first non-whitespace character on this line.
   If point was already at that position, move point to beginning of line."
-    (interactive)
-    (let ((oldpos (point)))
-      (back-to-indentation)
-      (and (= oldpos (point))
-           (beginning-of-line)))))
+  (interactive)
+  (let ((oldpos (point)))
+    (back-to-indentation)
+    (and (= oldpos (point))
+	 (beginning-of-line))))
+(global-set-key (kbd "C-m") 'smart-beginning-of-line)
 
 
 ;; Better view-mode
