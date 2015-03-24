80166ed48f787b5585214bd632f6777464f25910
Pedro Gomes Tavares <pedro.tavares@locaweb.com.br>
2015-03-24 15:55:01 -0300

    Remove Robe

--- init.el
+++ init.el
@@ -347,10 +347,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3c3836" "#fb4934" "#b8bb26" "#fabd2f" "#83a598" "#d3869b" "#8ec07c" "#ebdbb2"])
  '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t))))
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a655f17225ad0a7190c79602593563191b7640ddebbb8c8fbd80c9d82faff1c6" "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" default)))
+    ("75d807376ac43e6ac6ae3892f1f377a4a3ad2eb70b14223b4ed0355e62116814" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a655f17225ad0a7190c79602593563191b7640ddebbb8c8fbd80c9d82faff1c6" "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" default)))
  '(initial-buffer-choice "~/code"))
 ;; create the autosave dir if necessary, since emacs won't.
 (make-directory "~/.emacs.d/autosaves/" t)
@@ -837,3 +839,7 @@ Position the cursor at it's beginning, according to the current mode."
 ;; History minibuffer navigation
 (define-key minibuffer-local-map (kbd "C-p") 'previous-complete-history-element)
 (define-key minibuffer-local-map (kbd "C-n") 'next-complete-history-element)
+
+
+;; Projectile enable caching
+(setq projectile-enable-caching t)
