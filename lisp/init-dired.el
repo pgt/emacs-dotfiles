fdd4da8c6cfeac65df0d76f9b8a62078a9d8bcc7
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-04-26 17:06:09 -0300

    Show size of files on dired

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -7,8 +7,24 @@
           dired-do-copy
           dired-create-directory
           wdired-abort-changes)
-        (eval `(defadvice ,it (after revert-buffer activate)
-                 (revert-buffer))))
+  (eval `(defadvice ,it (after revert-buffer activate)
+	   (revert-buffer))))
+
+;;; Mark files and show the amount size
+(defun dired-get-size ()
+  (interactive)
+  (let ((files (dired-get-marked-files)))
+    (with-temp-buffer
+      (apply 'call-process "/usr/bin/du" nil t nil "-sch" files)
+      (message "Size of all marked files: %s"
+               (progn
+                 (re-search-backward "\\(^[0-9.,]+[A-Za-z]+\\).*total$")
+		 (match-string 1))))))
+
+(define-key dired-mode-map (kbd "?") 'dired-get-size)
+
+;;; Show Kbytes on dired
+(setq dired-listing-switches "-alh")
 
 (provide 'init-dired)
 ;;; init-dired.el ends here
