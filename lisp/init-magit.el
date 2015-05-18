21952fd052c755235207e4c443aa30ddf6a913d1
Pedro Gomes Tavares <pedro.tavares@locaweb.com.br>
2015-05-18 13:33:39 -0300

    New magit confs to open in fullscreen

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -11,5 +11,19 @@
 
 (setq magit-last-seen-setup-instructions "1.4.0")
 
+;; full screen magit-status
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
 (provide 'init-magit)
 ;;; init-magit.el ends here
