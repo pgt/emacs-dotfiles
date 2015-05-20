c71d1c5d7757044edd93dbce9d05119c4715a7d6
Pedro Gomes Tavares <pedro.tavares@locaweb.com.br>
2015-05-20 11:40:49 -0300

    Replace defun `smarter-move-beginning-of-line`

--- lisp/init-custom-defuns.el
+++ lisp/init-custom-defuns.el
@@ -129,17 +129,32 @@ Position the cursor at it's beginning, according to the current mode."
 
 
 ;; beggining of line
-(define-key global-map [remap move-beginning-of-line]
-  (defun smart-beginning-of-line ()
-    "Move point to first non-whitespace character or beginning-of-line.
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
 
-  Move point to the first non-whitespace character on this line.
-  If point was already at that position, move point to beginning of line."
-    (interactive)
-    (let ((oldpos (point)))
-      (back-to-indentation)
-      (and (= oldpos (point))
-           (beginning-of-line)))))
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+;; remap C-a to `smarter-move-beginning-of-line'
+(global-set-key [remap move-beginning-of-line]
+                'smarter-move-beginning-of-line)
 
 ;; Close buffer in other window
 (defun other-window-kill-buffer ()
