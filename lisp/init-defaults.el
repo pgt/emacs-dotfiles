36ad16653e3020b07c3a2c2678877aaac9aa3384
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-10-20 11:24:54 -0200

    Remove `ace-jump`

--- lisp/init-defaults.el
+++ lisp/init-defaults.el
@@ -93,33 +93,8 @@
 ;; Spelling
 (setq ispell-program-name "/usr/local/bin/aspell")
 
-
 ;; Reuse buffers between frames
 (setq-default display-buffer-reuse-frames t)
-
-;;
-;; ace jump mode major function
-;;
-(add-to-list 'load-path "~/.emacs.d/vendor/")
-(autoload
-  'ace-jump-mode
-  "ace-jump-mode"
-  "Emacs quick move minor mode"
-  t)
-;; you can select the key you prefer to
-(define-key global-map (kbd "C-c j") 'ace-jump-mode)
-
-;;
-;; enable a more powerful jump back function from ace jump mode
-;;
-(autoload
-  'ace-jump-mode-pop-mark
-  "ace-jump-mode"
-  "Ace jump back:-)"
-  t)
-(eval-after-load "ace-jump-mode"
-  '(ace-jump-mode-enable-mark-sync))
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode-pop-mark)
 
 ;; Helm AG
 (setq helm-ag-base-command "/usr/local/bin/ag --nocolor --nogroup --ignore-case")
