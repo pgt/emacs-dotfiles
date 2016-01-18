2d45ed36a35b333fc149f0b55c0b80b40a9324d3
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2016-01-18 18:09:11 -0200

    Add keybinding for `helm-find-files`

--- lisp/init-package-helm.el
+++ lisp/init-package-helm.el
@@ -34,6 +34,7 @@
 (global-set-key (kbd "s-t") 'helm-projectile-find-file)
 
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 (global-set-key (kbd "M-L") 'helm-projectile-switch-to-buffer) ;; open buffer of this project
 
